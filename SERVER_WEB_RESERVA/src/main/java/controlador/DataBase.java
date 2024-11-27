34f57f88ce4e671633a47995652b67c90984b368
DeiviDelaVega <wintereppo@gmail.com>
2024-11-27 16:15:26 -0500

    commit

---
+++ SERVER_WEB_RESERVA/src/main/java/controlador/DataBase.java
@@ -0,0 +1,40 @@
+package controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DataBase {
+	
+	
+	public static Connection getConnexion() {
+		
+		
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			
+			String conexion = "jdbc:mysql//localhost:3306/DB_Reserva_Alojamiento_Temporal";
+			String usuario = "root";
+			String contrasena = "alomomola890.";
+			
+			con = DriverManager.getConnection(conexion, usuario, contrasena);
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
+	
+}
---
+++ SERVER_WEB_RESERVA/src/main/java/modelo/BeanCliente.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class BeanCliente implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
